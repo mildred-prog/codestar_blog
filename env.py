3bd1d3baca1875399e3d695c9f2051a097806677
mildred-prog <mildylicious@gmail.com>
2025-03-25 17:30:28 +0000

    Update settings and add post option under django administration

--- env.py
+++ env.py
@@ -1,4 +1,9 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_g9UNHBdl0Drw@ep-weathered-brook-a2nkehow.eu-central-1.aws.neon.tech/raid_baked_blog_425385")
+    "DATABASE_URL", "postgresql://neondb_owner:npg_g9UNHBdl0Drw@ep-weathered-brook-a2nkehow.eu-central-1.aws.neon.tech/raid_baked_blog_425385"
+)
+
+os.environ.setdefault(
+    "SECRET_KEY", "django-insecure-fg0*l5sy83ok3gi&u33ds_fi_o=8j_d)n&zfbvwi@c!q(u(7m6"
+)
